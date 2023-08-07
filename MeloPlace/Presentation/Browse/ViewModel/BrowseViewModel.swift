c57fa071e4e3c4e5f039e6bf5792c134dec22156
yummyhawaiianpizzakim <yummyhawaiianpizzakim@gmail.com>
2023-08-07 19:52:16 +0900

    ViewModel - 생성

---
+++ MeloPlace/Presentation/Browse/ViewModel/BrowseViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  BrowseViewModel.swift
+//  MeloPlace
+//
+//  Created by 김요한 on 2023/08/07.
+//
+
+import Foundation
+
+class BrowseViewModel {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        let output = Output()
+        
+        return output
+    }
+}
---
+++ MeloPlace/Presentation/Map/ViewModel/MapViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewModel.swift
+//  MeloPlace
+//
+//  Created by 김요한 on 2023/08/07.
+//
+
+import Foundation
+
+class MapViewModel {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        let output = Output()
+        
+        return output
+    }
+}
