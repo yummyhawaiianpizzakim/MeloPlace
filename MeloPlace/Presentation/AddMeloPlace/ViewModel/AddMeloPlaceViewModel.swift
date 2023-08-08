fdf3477b8d74c6a6caa9346944542692960fefeb
yummyhawaiianpizzakim <yummyhawaiianpizzakim@gmail.com>
2023-08-08 18:16:38 +0900

    coordinator - addMeloPlaceCoordinator 생성 // addMeloPlaceViewController - 생성 // addMeloPlaceViewModel - 생성

---
+++ MeloPlace/Presentation/AddMeloPlace/ViewModel/AddMeloPlaceViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  AddMeloPlaceViewModel.swift
+//  MeloPlace
+//
+//  Created by 김요한 on 2023/08/08.
+//
+
+import Foundation
+
+class AddMeloPlaceViewModel {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        let output = Output()
+        
+        return output
+    }
+}
